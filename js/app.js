d23c045852553aab61ba10f29b77c475b90c3469
erguotou <erguotou525@gmail.com>
2015-11-10 23:42:36 +0800

    Lesson 1

--- js/app.js
+++ js/app.js
@@ -1,22 +1,22 @@
 angular.module('learningApp', ['ngRoute', 'hc.marked']).
   config(['$locationProvider', '$routeProvider', 'markedProvider',
     function($locationProvider, $routeProvider, markedProvider) {
-    $routeProvider.
+      $routeProvider.
         when('/introduction', {templateUrl: 'html/introduction.html'}).
         when('/getting-started', {templateUrl: 'html/getting-started.html'}).
         when('/lesson/:index', {templateUrl: 'html/lesson.html', controller: 'LessonCtrl'}).
         when('/the-end', {templateUrl: 'html/the-end.html'}).
         otherwise({redirectTo: '/introduction'});
-    markedProvider.setRenderer({
-      code: function (code, lang) {
-        return "<pre><code class='hljs"
-          + (lang?(" lang-" + lang):"") + "'>"
-          + hljs.highlightAuto(code).value + "</code></pre>";
-      }
-    });
-    markedProvider.setOptions({
-      gfm: true,
-      tables: true,
-      breaks: true
-    });
+      markedProvider.setRenderer({
+        code: function (code, lang) {
+          return "<pre><code class='hljs"
+            + (lang?(" lang-" + lang):"") + "'>"
+            + hljs.highlightAuto(code).value + "</code></pre>";
+        }
+      });
+      markedProvider.setOptions({
+        gfm: true,
+        tables: true,
+        breaks: true
+      });
   }]);
